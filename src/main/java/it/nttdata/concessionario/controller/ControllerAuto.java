97bf39968af2dfb8970b681df350e6f0fb89cf79
Raoabi <rymm98@gmail.com>
2021-06-11 12:46:38 +0200

    aggiornato

---
+++ src/main/java/it/nttdata/concessionario/controller/ControllerAuto.java
@@ -0,0 +1,24 @@
+package it.nttdata.concessionario.controller;
+
+import it.nttdata.concessionario.repository.AutoRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControllerAuto {
+
+    private final AutoRepository autoRepository;
+
+    public ControllerAuto(AutoRepository autoRepository) {
+        this.autoRepository = autoRepository;
+    }
+
+    @GetMapping("/automobili")
+    public String getAuto(Model model){
+        model.addAttribute("automobili",autoRepository.findAll());
+        
+        return "auto";
+    }
+
+}
