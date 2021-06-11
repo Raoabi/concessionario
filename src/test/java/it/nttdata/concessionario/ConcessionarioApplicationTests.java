38b983db3b14f5928f96c22eee6202fb980f4e00
Raoabi <rymm98@gmail.com>
2021-06-11 11:30:56 +0200

    Initial commit

---
+++ src/test/java/it/nttdata/concessionario/ConcessionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package it.nttdata.concessionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcessionarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
